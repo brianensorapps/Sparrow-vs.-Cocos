5358c99df11eedc72d274887be772ff22fe7de70
Arend Hintze <ahintze@mac.com>
2011-09-14 07:32:43 -0400

    I played around with speed, no essential change
    as said above no actual change but the software forces me to commit
    before I update

--- Source/Classes/Game.m
+++ Source/Classes/Game.m
@@ -75,6 +75,7 @@
 }
 
 - (void)onEnterFrame:(SPEnterFrameEvent *)event {
+<<<<<<< HEAD
     [gameJuggler advanceTime:event.passedTime];
     world.pivotX += -sin(world.rotation)*(100*event.passedTime);
     world.pivotY += -cos(world.rotation)*(100*event.passedTime);
@@ -94,6 +95,11 @@
         [gameJuggler removeAllObjects];
         [[gameJuggler delayInvocationAtTarget:self byTime:5] setInvertedControls:NO];
     }
+=======
+    map.pivotX += -sin(map.rotation)*(100*event.passedTime);
+    map.pivotY += -cos(map.rotation)*(100*event.passedTime);
+    
+>>>>>>> I played around with speed, no essential change
     if (turning) {
         switch (turnDirection) {
             case 0:
